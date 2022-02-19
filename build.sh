1af114c662058ec08f290f59ea3239c3ff4c2b38
“tsharp0x11” <ibuki042004@gmail.com>
2022-02-19 12:10:08 +0900

    removed build.sh

--- build.sh
+++
@@ -1,5 +0,0 @@
-
-tsp torth.tsp
-
-/usr/local/Cellar/nasm/2.15.05/bin/nasm -f macho64 torth.asm && ld torth.o -o torth -macosx_version_min 11.4 -L /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib -lSystem -no_pie
-
